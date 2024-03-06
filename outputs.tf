2b1f08026ace3e79b90e2cd4d1dc7295b4884c59
Maksym Bilyi <w0957316192@gmail.com>
2024-03-06 17:52:33 +0200

    create deploy1

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.lms.name
 }
 
 output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+  value = azurerm_linux_virtual_machine.lms_terraform_vm.public_ip_address
 }
 
 output "tls_private_key" {
