b15277ef81f54cc39f2f6e190c66c696b2314ce9
bwhite04 <60192676+bwhite04@users.noreply.github.com>
2024-03-06 21:59:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "resource_group_location" {
   description = "Місце розташування ресурсної групи"
   type        = string
-  default     = "UK South"  # Вкажіть своє бажане місце розташування за замовчуванням
+  default     = "UK South"  
 }
 
 variable "service_name" {
   description = "Назва сервісу"
   type        = string
-  default     = "Frape-terraform"  # Вкажіть свою бажану назву сервісу за замовчуванням
+  default     = "Frape-terraform"  
 }
 
 variable "admin_username" {
   description = "Ім'я адміністратора для SSH"
   type        = string
-  default = "secrets.AZURE_USER"   # Вкажіть своє бажане ім'я користувача за замовчуванням
+  default = "secrets.AZURE_USER" 
 }
 
 variable "admin_password" {
   description = "Пароль адміністратора для SSH"
   type        = string
-  default = "secrets.AZURE_PASS_USER" # Вкажіть свій бажаний пароль за замовчування
+  default = "secrets.AZURE_PASS_USER" 
 }
